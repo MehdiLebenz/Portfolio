6acca9e79671e1384d39c3cd34d67dfec883edf8
MehdiLebenz <mehdilebenz@gmail.com>
2021-05-13 18:22:13 +0100

    add contact page

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,8 +32,8 @@ module.exports = {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#3099b0`,
+        theme_color: `#3099b0`,
         display: `minimal-ui`,
         icon: `src/images/profile.png`, // This path is relative to the root of the site.
       },

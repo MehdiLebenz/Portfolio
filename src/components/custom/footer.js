fc340065b95547c99bf29be77018762a76fd0e11
MehdiLebenz <mehdilebenz@gmail.com>
2021-11-02 18:43:48 +0100

    fix layout

--- src/components/custom/footer.js
+++ src/components/custom/footer.js
@@ -35,6 +35,7 @@ const Fotter = () => {
         </div>
       </div>
     </div>
+    
     </FooterWrapper>
   )
 }
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -2,13 +2,17 @@ import React from 'react';
 import Header from "../components/header/header";
 import Projects from '../components/projects/project'
 import Layout from "../components/layout"
+import Header from '../components/header/header'
+import Footer from '../components/custom/footer'
 
 const ProjectsPage = () => {
   
     return (
-<Layout>
+      <div>
+      <Header/>
 <Projects/>
-</Layout>
+<Footer/>
+</div>
 
 
     

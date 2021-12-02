ce6b3e7e4545f5f345ab3771e8a3caa41fd45055
MehdiLebenz <mehdilebenz@gmail.com>
2021-12-02 19:47:31 +0100

    change project name

--- src/components/projects/project.jsx
+++ src/components/projects/project.jsx
@@ -1,16 +1,16 @@
-import React from 'react';
-import Wrapper from './WrapperProject';
-import lactalis from '../../images/projects/lactalis.png';
-import porsche from '../../images/porsche.png';
-import carrefour from '../../images/projects/carrefour.svg';
-import vyv from '../../images/projects/vyv.png';
-import hyperspread from '../../images/projects/hyperspread.jpeg';
-import monoprix from '../../images/projects/monoprix.png';
-import lacoste from '../../images/projects/lacoste.png';
-import Volkswagen from '../../images/projects/Volkswagen.png';
-import '../layout.css';
+import React from 'react'
+import Wrapper from './WrapperProject'
+import lactalis from '../../images/projects/lactalis.png'
+import porsche from '../../images/porsche.png'
+import carrefour from '../../images/projects/carrefour.svg'
+import vyv from '../../images/projects/vyv.png'
+import hyperspread from '../../images/projects/hyperspread.jpeg'
+import monoprix from '../../images/projects/monoprix.png'
+import lacoste from '../../images/projects/lacoste.png'
+import Volkswagen from '../../images/projects/Volkswagen.png'
+import '../layout.css'
 
-const Projects = function projects () {
+const Projects = function projects() {
   return (
     <Wrapper>
       <div className="container">
@@ -20,10 +20,14 @@ const Projects = function projects () {
               <img className="image" src={lactalis} />
             </div>
             <div className="card-info">
-              <h2 className="card-title">Lactalis - Technical project manager (2021) </h2>
+              <h2 className="card-title">
+                ABCDE{' '}
+              </h2>
               <p className="card-intro">
-                mysql database transfer to Postgresql , development of dedicated microservice to manage the conditions of import and export, connecting of daily flows and verification of all conditions, for BI side connecting Domo and server management
-                .
+                mysql database transfer to Postgresql , development of dedicated
+                microservice to manage the conditions of import and export,
+                connecting of daily flows and verification of all conditions,
+                for BI side connecting Domo and server management .
               </p>
             </div>
           </div>
@@ -34,7 +38,10 @@ const Projects = function projects () {
               <img className="image" src={porsche} />
             </div>
             <div className="card-info">
-              <h2 className="card-title"> Porsche France - Project Director (2020)</h2>
+              <h2 className="card-title">
+                {' '}
+                Porsche France - Project Director (2020)
+              </h2>
               <p className="card-intro">
                 Lead Management mobile application
                 <br />
@@ -48,13 +55,12 @@ const Projects = function projects () {
               <img className="image" src={carrefour} />
             </div>
             <div className="card-info">
-              <h2 className="card-title">Scrum Master - Carrefour Nova (2019)</h2>
+              <h2 className="card-title">
+                Scrum Master - Carrefour Nova (2019)
+              </h2>
               <p className="card-intro">
                 Mobile application
-                <br />
-                {' '}
-                #react-native #nodejs #GCP #graphql
-                {' '}
+                <br /> #react-native #nodejs #GCP #graphql{' '}
               </p>
             </div>
           </div>
@@ -65,9 +71,12 @@ const Projects = function projects () {
               <img className="image" src={monoprix} />
             </div>
             <div className="card-info">
-              <h2 className="card-title">Monoprix France - Project Manager (2018) </h2>
+              <h2 className="card-title">
+                Monoprix France - Project Manager (2018){' '}
+              </h2>
               <p className="card-intro">
-                Complete overhaul of the monoprix site, order funnel, product management, payment, stock management, delivery management..
+                Complete overhaul of the monoprix site, order funnel, product
+                management, payment, stock management, delivery management..
                 <br />
                 #Reactjs #Apollo #Nodejs #GCP #GRAPHQL #Microservices
               </p>
@@ -80,13 +89,12 @@ const Projects = function projects () {
               <img className="image" src={hyperspread} />
             </div>
             <div className="card-info">
-              <h2 className="card-title">HyperSpread - Project Director (2020) </h2>
+              <h2 className="card-title">
+                HyperSpread - Project Director (2020){' '}
+              </h2>
               <p className="card-intro">
                 First Saas dedicated to local marketing in France
-                <br />
-                {' '}
-                #REACT #NODE #MICROSERVICES #GRAPHQL #GRAPHQL
-                {' '}
+                <br /> #REACT #NODE #MICROSERVICES #GRAPHQL #GRAPHQL{' '}
               </p>
             </div>
           </div>
@@ -112,8 +120,14 @@ const Projects = function projects () {
               <img className="image" src={lacoste} />
             </div>
             <div className="card-info">
-              <h2 className="card-title">Lacoste - Technical Project Manager (2021) </h2>
-              <p className="card-intro"> Digital sales experience in 2 Showrooms (Paris / Shanghai) for their wholesaler..</p>
+              <h2 className="card-title">
+                Lacoste - Technical Project Manager (2021){' '}
+              </h2>
+              <p className="card-intro">
+                {' '}
+                Digital sales experience in 2 Showrooms (Paris / Shanghai) for
+                their wholesaler..
+              </p>
             </div>
           </div>
         </div>
@@ -125,19 +139,17 @@ const Projects = function projects () {
             <div className="card-info">
               <h2 className="card-title">1 Day routes for this weekend</h2>
               <p className="card-intro">
-                Creation of 2 distinct platforms (ACCESSORIES SITES, MONTHLY FILE) to revitalize the commercial dynamics of the "DPS" parts and services department.
-                <br />
-                {' '}
-                #SHOPIFY #NEXTJS #VERCEL #NODEJS #PIMCORE
-                {' '}
+                Creation of 2 distinct platforms (ACCESSORIES SITES, MONTHLY
+                FILE) to revitalize the commercial dynamics of the "DPS" parts
+                and services department.
+                <br /> #SHOPIFY #NEXTJS #VERCEL #NODEJS #PIMCORE{' '}
               </p>
             </div>
           </div>
         </div>
       </div>
     </Wrapper>
+  )
+}
 
-  );
-};
-
-export default Projects;
+export default Projects
